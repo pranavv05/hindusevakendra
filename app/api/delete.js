2d7727307e2ac0800ff7934d30a3ca842305fa84
Pranav Vatsa <142565246+pranavv05@users.noreply.github.com>
2025-07-13 22:04:21 +0530

    Update delete.js

--- app/api/delete.js
+++ app/api/delete.js
@@ -1,13 +1,1 @@
-import dbConnect from '../../lib/dbConnect';
-import User from '../../models/User';
 
-export default async function handler(req, res) {
-  await dbConnect();
-
-  const result = await User.deleteMany({ name: "Pranav Vatsa" });
-
-  res.status(200).json({
-    message: "Deleted all users with name 'Pranav Vatsa'",
-    deletedCount: result.deletedCount,
-  });
-}
